25061c31ec70911050770c4af986c43420bcdc32
santosandrey146@gmail.com <@ndrey1212AA>
2026-09-08 17:23:41 -0300

    Commit Inicial

---
+++ src/main/java/com/backend/ContadorDeImpactos/ContadorDeImpactosApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ContadorDeImpactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadorDeImpactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadorDeImpactosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/ContadorDeImpactos/ContadorDeImpactosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ContadorDeImpactos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorDeImpactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
